29690ead484a28b9d6d6ceae71b424eba59e17b5
leticia <leticiapintoalva@gmail.com>
2015-09-22 13:42:27 -0500

    Esta es la primera version

---
+++ config.php
@@ -0,0 +1,32 @@
+<?PHP
+/* CONFIG SETTINGS - change this */
+$siteTitle = 'Climate Station'; /* will be displayed above the url-bar */
+$siteName = 'Estación Meteorológica usando RaspberryPi'; /* The biggest title on your homepage */
+$siteDesc ='Mide temperatura y humedad visualizados en un browser haciendo un trazado de gráficos en tiempo real '; /*subtitle */
+$siteTitleHome = 'Home'; // will be displayed above the url-bar when the home-page is open
+$siteFooter = '©your name';
+
+$siteMetaDesc = 'A description of your site for Google here';
+$siteMetaKeywords = 'Some, keywords, seperated, by, kommas, here, max 10';
+
+$theme = "theme_default"; // name of the subfolder in themes directory for the theme you want
+
+/*MOBILE VERSION NOT IN LITE EDITION OF THIS TEMPLATE. Go to http://metro-webdesign.info and donate to get the full version!*/
+
+
+
+/*Must be the same as the pageLink array in js/config.js ! For SEO optimization (tip: first create the array in config.js, then copy it here and place a $-sign in front of every line */
+$pageLink = array();
+$pageLink['Typography'] = 'typography.php';
+$pageLink['Slider example'] = 'sliders.php';
+$pageLink['Images 1'] = 'myimages.html';
+$pageLink['Images 2'] = 'otherimages.php';
+$pageLink['Images 3'] = 'images3.php';
+$pageLink['Terms of service'] = 'tos.php';
+
+/*YOUR LOGIN FOR ADMIN SECTION, access it on http:/yoursite.com/admin/ */
+/*Admin section not supported in lite edition!*/
+
+/*COMPRESSING */
+/*Compression not supported in lite edition */
+?>
